cc668e4eeb0984b9c96c7120f871eb3009d7b747
Anton <micvant@mail.ru>
2020-08-28 16:58:41 +0900

    add menu and btn

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    public GameObject shopPanel;
     public Text scoreText;
-    public int score;
+    public Text btnTextUpgrade;
+
+    [Header("Массив цен улучшений")]
+    
+    private int shopCost = 100;
+    private int index = 0;
+    private int bonus = 1;
+    private int score;
+
+    // ↓ Обновление каждого кадра + Обновление счета
+    void Update()
+    {
+        scoreText.text = score + " $";
+        btnTextUpgrade.text = "Buy + 1 click for " + shopCost + " $";
+    }
+
+    // ↓ Покупка улучшения клика
+    public void buyUpgrade()
+    {
+        if(score >= shopCost)
+        {
+            bonus ++;
+            score -= shopCost;
+            index++;
+            // ↓ Увеличиваем цену в 2 раза 
+            shopCost *= 2;
+        }  else
+        {
+            // ↓ Добавить вывод текста на экран "Не хватает денег!"
+        }
+    }
+
+    // ↓ Открытие/Закрытие магазина улучшений
+    public void openShop()
+    {
+        shopPanel.SetActive(!shopPanel.activeSelf);
+    }
+
+    // ↓ При клике ↓
     public void OnClick()
     {        
-        score++;
-        scoreText.text = score + " $";
+        score += bonus;
     }
 }

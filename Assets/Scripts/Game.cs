d5f0c46dfb36f218e1b56a8dcd005424845fd778
Anton <micvant@mail.ru>
2020-08-28 11:40:01 +0900

    add scripts Game &  UI

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    public void OnClick()
+    {        
+        score++;
+        scoreText.text = score + " $";
+    }
+}

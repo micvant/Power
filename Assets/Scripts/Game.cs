5c6237adaf3a203b882a9bec1f6c74cbcf949fe6
Anton <micvant@mail.ru>
2020-09-04 17:41:17 +0900

    add new product in shop & del trash

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,24 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Threading;
 
 public class Game : MonoBehaviour
 {
     public GameObject shopPanel;
     public Text scoreText;
     public Text btnTextUpgrade;
+    public Text btnTextAutoClick;
 
-    [Header("Массив цен улучшений")]
-    
+    [Header("Массив цен улучшений")]    
+    [Header("Улучшение клика")]
     private int shopCost = 100;
+    [Header("Улучшение АвтоКлика")]
+    private int costAutoClick = 10;
+
     private int index = 0;
     private int bonus = 1;
     private int score;
-
+    [Header("АвтоКликеры")]
+    private int VacuumCounts;
+    void Start()
+    {
+        StartCoroutine(BonusPerSecond());
+    }
     // ↓ Обновление каждого кадра + Обновление счета
     void Update()
     {
         scoreText.text = score + " $";
-        btnTextUpgrade.text = "Buy + 1 click " + shopCost + " $";
+        btnTextUpgrade.text = "Купить " + shopCost + " $";
+        btnTextAutoClick.text = "Купить " + costAutoClick + " $";
     }
 
     // ↓ Покупка улучшения клика
@@ -26,7 +38,9 @@ public class Game : MonoBehaviour
     {
         if(score >= shopCost)
         {
-            bonus ++;
+            // ↓ Добавляем +1$ при Клике
+            bonus++;
+            // ↓ Забираем деньги за покупку
             score -= shopCost;
             index++;
             // ↓ Увеличиваем цену в 2 раза 
@@ -41,6 +55,33 @@ public class Game : MonoBehaviour
     public void openShop()
     {
         shopPanel.SetActive(!shopPanel.activeSelf);
+    }
+
+    // Покупка ↓ АвтоКлика
+    public void BuyVacuum()
+    {
+        if(score >= costAutoClick)
+        {
+            // ↓ Добавляем +1$ АвтоКлик
+            VacuumCounts++;
+            // ↓ Забираем деньги за покупку
+            score -= costAutoClick;
+            // ↓ Увеличиваем цену в 2 раза 
+            costAutoClick *= 2;
+        }else
+        {
+            // ↓ Добавить вывод текста на экран "Не хватает денег!"
+        }
+    }
+
+    // ↓ Прибавляет $ в зависимости от количества VacuumCounts
+    IEnumerator BonusPerSecond()
+    {
+        while (true)
+        {
+            score += VacuumCounts;
+            yield return new WaitForSeconds(1);
+        }
     }
 
     // ↓ При клике увеличивает счет(score) 
